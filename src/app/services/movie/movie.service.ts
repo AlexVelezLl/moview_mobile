a80fef55c3c779272dcc1bd00d3120b0dc63677f
AlexVelezLl <alanvele@espol.edu.ec>
2021-09-06 15:31:02 -0500

    Showing and sorting reviews by date

--- src/app/services/movie/movie.service.ts
+++ src/app/services/movie/movie.service.ts
@@ -20,23 +20,46 @@ export class MovieService {
     return this.http.get<Movie>(url_movie).toPromise();
   }
 
-  getMovieReviews(id: number): Promise<Review[]> {
+  async getMovieReviews(id: number): Promise<Review[]> {
     const url_reviews = environment.api + '/review/movie/' + id;
-    return this.http.get<Review[]>(url_reviews).toPromise();
+    const reviews = await this.http.get<Review[]>(url_reviews).toPromise();
+    const reviewsSorted = reviews.sort((a, b) => {
+      return new Date(b.date).getTime() - new Date(a.date).getTime();
+    });
+    this.formatReviewDate(reviewsSorted);
+    return reviewsSorted;
   }
 
-  getUserReviews(id: number): Promise<Review[]> {
+  async getUserReviews(id: number): Promise<Review[]> {
     const url_reviews = environment.api + '/review/user/' + id;
-    return this.http.get<Review[]>(url_reviews).toPromise();
+    const reviews = await this.http.get<Review[]>(url_reviews).toPromise();
+    const reviewsSorted = reviews.sort((a, b) => {
+      return new Date(b.date).getTime() - new Date(a.date).getTime();
+    });
+    this.formatReviewDate(reviewsSorted);
+    return reviewsSorted;
   }
 
-  getUserFollowingReviews(id: number): Promise<Review[]> {
+  async getUserFollowingReviews(id: number): Promise<Review[]> {
     const url_reviews = environment.api + '/review/following';
-    return this.http.get<Review[]>(url_reviews).toPromise();
+    const reviews = await this.http.get<Review[]>(url_reviews).toPromise();
+    const reviewsSorted = reviews.sort((a, b) => {
+      return new Date(b.date).getTime() - new Date(a.date).getTime();
+    });
+    this.formatReviewDate(reviewsSorted);
+    return reviewsSorted;
   }
 
   addReview(review: Review) {
     const url_review = environment.api + '/review';
     return this.http.post(url_review, review).toPromise();
+  }
+
+  private formatReviewDate(reviews: Review[]) {
+    reviews.forEach((review) => {
+      const date = review.date.split('/');
+      const year = date[2].substring(2, 4);
+      review.date = `${date[0]}/${date[1]}/${year}`;
+    });
   }
 }
